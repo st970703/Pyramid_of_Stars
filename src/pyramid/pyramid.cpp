d5f7112a6e26986a6fbeb6f18aaa720518f81b20
Mike Lee <st970703@gmail.com>
2026-07-01 16:53:19 +1000

    Extracted if condition to a bool

--- src/pyramid/pyramid.cpp
+++ src/pyramid/pyramid.cpp
@@ -5,7 +5,8 @@ QString buildPyramid(int height, PyramidDisplayMode displayMode) {
 
     for (int i = 1; i <= height; i++) {
         for (int j = 1; j <= 2*height - 1; j++) {
-            if (j >= height-(i-1) && j <= height+(i-1)) {
+            bool inside_pyramid_shape = j >= height-(i-1) && j <= height+(i-1);
+            if (inside_pyramid_shape) {
                 if (displayMode == PyramidDisplayMode::Stars) {
                     pyramid += QString('*');
                 }
