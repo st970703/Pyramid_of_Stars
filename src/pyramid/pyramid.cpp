b671db8efe3ab9bac2ffe19b969c2972484968b0
Mike Lee <st970703@gmail.com>
2026-07-01 17:06:06 +1000

    Use switch statement

--- src/pyramid/pyramid.cpp
+++ src/pyramid/pyramid.cpp
@@ -7,11 +7,15 @@ QString buildPyramid(int height, PyramidDisplayMode displayMode) {
         for (int j = 1; j <= 2*height - 1; j++) {
             bool inside_pyramid_shape = j >= height-(i-1) && j <= height+(i-1);
             if (inside_pyramid_shape) {
-                if (displayMode == PyramidDisplayMode::Stars) {
-                    pyramid += QString('*');
-                }
-                else {
-                    pyramid += QString::number(i);
+                switch (displayMode) {
+                    case PyramidDisplayMode::Stars:
+                        pyramid += QString('*');
+                        break;
+                    case PyramidDisplayMode::Numbers:
+                        pyramid += QString::number(i);
+                        break;
+                    default:
+                        throw std::invalid_argument("Invalid display mode selected!");
                 }
             }
             else {
